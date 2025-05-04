3fcbeea4b212ff0952c25da1b04c101b0d7e7470
SANTIAGO RUEDA DAVID <santiagorueda1116442@correo.itm.edu.co>
2025-05-04 16:41:56 -0500

    Adicion de responsables dinamicamente al formulario de indicadores

--- BlazorFront/Services/IRepository.cs
+++ BlazorFront/Services/IRepository.cs
@@ -3,7 +3,7 @@
     public interface IRepository
     {
         Task<T> GetAsync<T>(string url);
-        Task<object> PostAsync<T>(string url, T entity);
+        Task<T> PostAsync<T>(string url, T entity);
         Task<object> PutAsync<T>(string url, T entity);
         Task<T> GetByIDAsync<T>(string url, int id);
         Task<T> GetByIDAsync<T>(string url, string id);
